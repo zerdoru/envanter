ce2bf57608246e6a70f3c4eb976b77af02126ea1
zerdoru <zerdoru@yandex.com>
2019-09-19 11:19:27 +0300

    Implement the basic user registration

--- Envanter/Envanter/UserRegister.Designer.cs
+++ Envanter/Envanter/UserRegister.Designer.cs
@@ -28,11 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
             this.buttonRegister = new System.Windows.Forms.Button();
             this.buttonCancel = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -42,79 +42,81 @@
             this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // textBox1
+            // textBoxUsername
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.textBox1.Location = new System.Drawing.Point(96, 12);
-            this.textBox1.MaxLength = 50;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(142, 24);
-            this.textBox1.TabIndex = 0;
+            this.textBoxUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.textBoxUsername.Location = new System.Drawing.Point(112, 14);
+            this.textBoxUsername.MaxLength = 50;
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(165, 24);
+            this.textBoxUsername.TabIndex = 0;
             // 
-            // textBox2
+            // textBoxPassword
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.textBox2.Location = new System.Drawing.Point(96, 42);
-            this.textBox2.MaxLength = 50;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(142, 24);
-            this.textBox2.TabIndex = 1;
+            this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.textBoxPassword.Location = new System.Drawing.Point(112, 48);
+            this.textBoxPassword.MaxLength = 50;
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(165, 24);
+            this.textBoxPassword.TabIndex = 1;
             // 
-            // textBox3
+            // textBoxEmail
             // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.textBox3.Location = new System.Drawing.Point(96, 72);
-            this.textBox3.MaxLength = 320;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(142, 24);
-            this.textBox3.TabIndex = 2;
+            this.textBoxEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.textBoxEmail.Location = new System.Drawing.Point(112, 83);
+            this.textBoxEmail.MaxLength = 320;
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(165, 24);
+            this.textBoxEmail.TabIndex = 2;
             // 
-            // textBox4
+            // textBoxFirstName
             // 
-            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.textBox4.Location = new System.Drawing.Point(96, 102);
-            this.textBox4.MaxLength = 50;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(142, 24);
-            this.textBox4.TabIndex = 3;
+            this.textBoxFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.textBoxFirstName.Location = new System.Drawing.Point(112, 118);
+            this.textBoxFirstName.MaxLength = 50;
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(165, 24);
+            this.textBoxFirstName.TabIndex = 3;
             // 
-            // textBox5
+            // textBoxLastName
             // 
-            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.textBox5.Location = new System.Drawing.Point(96, 132);
-            this.textBox5.MaxLength = 50;
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(142, 24);
-            this.textBox5.TabIndex = 4;
+            this.textBoxLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.textBoxLastName.Location = new System.Drawing.Point(112, 152);
+            this.textBoxLastName.MaxLength = 50;
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(165, 24);
+            this.textBoxLastName.TabIndex = 4;
             // 
             // buttonRegister
             // 
             this.buttonRegister.AutoSize = true;
             this.buttonRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.buttonRegister.Location = new System.Drawing.Point(96, 185);
+            this.buttonRegister.Location = new System.Drawing.Point(112, 213);
             this.buttonRegister.Name = "buttonRegister";
-            this.buttonRegister.Size = new System.Drawing.Size(142, 28);
+            this.buttonRegister.Size = new System.Drawing.Size(166, 32);
             this.buttonRegister.TabIndex = 5;
             this.buttonRegister.Text = "Register";
             this.buttonRegister.UseVisualStyleBackColor = true;
+            this.buttonRegister.Click += new System.EventHandler(this.buttonRegister_Click);
             // 
             // buttonCancel
             // 
             this.buttonCancel.AutoSize = true;
             this.buttonCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.buttonCancel.Location = new System.Drawing.Point(12, 185);
+            this.buttonCancel.Location = new System.Drawing.Point(14, 213);
             this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(78, 28);
+            this.buttonCancel.Size = new System.Drawing.Size(91, 32);
             this.buttonCancel.TabIndex = 6;
             this.buttonCancel.Text = "Cancel";
             this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Location = new System.Drawing.Point(14, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(77, 18);
             this.label1.TabIndex = 7;
@@ -124,7 +126,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Location = new System.Drawing.Point(14, 52);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(75, 18);
             this.label2.TabIndex = 8;
@@ -134,7 +136,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.label3.Location = new System.Drawing.Point(12, 75);
+            this.label3.Location = new System.Drawing.Point(14, 87);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(50, 18);
             this.label3.TabIndex = 9;
@@ -144,7 +146,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.label4.Location = new System.Drawing.Point(12, 105);
+            this.label4.Location = new System.Drawing.Point(14, 121);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(78, 18);
             this.label4.TabIndex = 10;
@@ -154,7 +156,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.label5.Location = new System.Drawing.Point(12, 135);
+            this.label5.Location = new System.Drawing.Point(14, 156);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(77, 18);
             this.label5.TabIndex = 11;
@@ -162,9 +164,9 @@
             // 
             // UserRegister
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(250, 225);
+            this.ClientSize = new System.Drawing.Size(292, 260);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -172,26 +174,21 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonRegister);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "UserRegister";
             this.Text = "Registration";
+            this.Load += new System.EventHandler(this.UserRegister_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Button buttonRegister;
         private System.Windows.Forms.Button buttonCancel;
         private System.Windows.Forms.Label label1;
@@ -199,5 +196,10 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxUsername;
     }
 }
--- Envanter/Envanter/UserRegister.cs
+++ Envanter/Envanter/UserRegister.cs
@@ -1,12 +1,59 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace Envanter
 {
     public partial class UserRegister : Form
     {
+        private readonly SqlConnection _connection =
+            new SqlConnection(
+                @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\bilgiislem\source\repos\envanter\Envanter\Envanter\Inventory.mdf;Integrated Security=True");
+
         public UserRegister()
         {
             InitializeComponent();
+        }
+
+        private void UserRegister_Load(object sender, EventArgs e)
+        {
+            if (_connection.State != ConnectionState.Open) _connection.Open();
+        }
+
+        private void buttonRegister_Click(object sender, EventArgs e)
+        {
+            var cmd = _connection.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText =
+                $"INSERT INTO Registration (Username, Password, Email, FirstName, LastName) VALUES ('{textBoxUsername.Text}', '{textBoxPassword.Text}', '{textBoxEmail.Text}', '{textBoxFirstName.Text}', '{textBoxLastName.Text}')";
+
+            try
+            {
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException exception)
+            {
+                switch (exception.Number)
+                {
+                    case 2601:
+                    case 2627:
+                        MessageBox.Show("The e-mail address or username you entered is already in use");
+                        break;
+                    default:
+                        MessageBox.Show("Error while saving data");
+                        break;
+                }
+
+                return;
+            }
+
+            MessageBox.Show("Registration successfull");
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
